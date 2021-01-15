00fa87bc995763cc54553095d9db6d369b81bbd2
alvarolopez99 <48553320+alvarolopez99@users.noreply.github.com>
2021-01-15 19:41:20 +0100

    Vuelta dada a los mapas

--- PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
+++ PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.practicafinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    Button btn_openMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_openMap = (Button) findViewById(R.id.btn_openMap);
+
+        btn_openMap.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btn_openMap:
+                Intent mapa = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(mapa);
+                break;
+        }
     }
 }
