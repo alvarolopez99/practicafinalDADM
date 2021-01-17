ba115d5ecf430a563b2e7ca806380baf742a59be
alvarolopez99 <48553320+alvarolopez99@users.noreply.github.com>
2021-01-17 20:36:38 +0100

    Update MapsActivity.java

--- PracticaFinal/app/src/main/java/com/example/practicafinal/MapsActivity.java
+++ PracticaFinal/app/src/main/java/com/example/practicafinal/MapsActivity.java
@@ -5,14 +5,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
-<<<<<<< HEAD
-<<<<<<< HEAD
 import android.content.Context;
 import android.content.Intent;
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -41,8 +35,6 @@ import java.util.List;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     private Button btn_Satelite, btn_Hibrido, btn_Normal, btn_Terreno;
 
@@ -51,18 +43,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Marker marcador;
     double lat = 0.0;
     double lng = 0.0;
-
-=======
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
-    private Button btn_Satelite;
-    private Button btn_Hibrido;
-    private Button btn_Normal;
-    private Button btn_Terreno;
-<<<<<<< HEAD
->>>>>>> parent of ed76390... Compartir en Redes Sociales
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,17 +59,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         btn_Normal = (Button) findViewById(R.id.id_Normal);
         btn_Terreno = (Button) findViewById(R.id.id_Terreno);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         btn_Twitter = (Button) findViewById(R.id.id_Twitter);
         btn_Facebook = (Button) findViewById(R.id.id_Facebook);
         btn_Whatsapp = (Button) findViewById(R.id.id_WhatsApp);
 
-
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
         btn_Satelite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -119,9 +92,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
         btn_Twitter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -154,12 +124,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startActivity(Intent.createChooser(intent, "Compartir..."));
             }
         });
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
-=======
->>>>>>> parent of ed76390... Compartir en Redes Sociales
-
-
     }
 
     public void addMarker(GoogleMap googleMap) {
